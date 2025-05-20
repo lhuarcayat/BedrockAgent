7bceb6320922cad5e66e096a6a37879e055adbb2
Rolando Quispe <rolando.quispe@applying.cloud>
2025-05-20 15:26:45 -0500

    Feat: changed main for correct use for lambda and sqs items

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
-module "s3" {
-  source       = "./modules/s3"
-  project_prefix = "par-servicios-poc"
-  tags         = ["par-servicios", "agents-bedrock"]
-}
+# module "s3" {
+#   source       = "./modules/s3"
+#   project_prefix = "par-servicios-poc"
+#   tags         = ["par-servicios", "agents-bedrock"]
+# }
 
 module "sqs" {
   source       = "./modules/sqs"
@@ -12,24 +12,24 @@ module "sqs" {
 
 module "classification_lambda" {
   source       = "./modules/lambda-wrapper"
-  project_prefix = "par-servicios-poc"
-  function_name = "classification"
+  source_path  = "${path.module}/../functions/classification"
+  project_prefix = var.project_prefix
+  function_name = "${var.stage_name}-classification"
   handler      = "src/lambda_function.lambda_handler"
-  runtime      = "python3.12"
   environment = {
-    S3_BUCKET      = module.s3.raw_bucket_name
+    # S3_BUCKET      = module.s3.raw_bucket_name
     EXTRACTION_SQS = module.sqs.extraction_queue_url
   }
 }
 
 module "extraction_scoring_lambda" {
   source       = "./modules/lambda-wrapper"
+  source_path  = "${path.module}/../functions/extraction-scoring"
   project_prefix = "par-servicios-poc"
   function_name = "extraction-scoring"
   handler      = "src/lambda_function.lambda_handler"
-  runtime      = "python3.12"
   environment = {
-    S3_BUCKET    = module.s3.processed_bucket_name
+    # S3_BUCKET    = module.s3.processed_bucket_name
     FALLBACK_SQS = module.sqs.fallback_queue_url
   }
 }
